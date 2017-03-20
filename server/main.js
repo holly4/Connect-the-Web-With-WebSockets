e6ceb50230fe3fcbf99b6fbe4a76250386b2cb77
Holly <hollies@hollies.pw>
2017-03-19 21:27:01 -0700

    complete video 2.4

--- server/main.js
+++ server/main.js
@@ -4,14 +4,17 @@ var server = require("http").Server(app);
 var port = 80;
 var io = require("socket.io")(server);
 
-app.get("/", function (req, res) {
-    var date = new Date();
-    res.send("Hello at " + date.toISOString());
-    console.log("connected to express.");
-});
+
+//app.get("/", function (req, res) {
+//    var date = new Date();
+//    res.send("Hello at " + date.toISOString());
+//    console.log("connected to express.");
+//});
+
+app.use(express.static('app'));
 
 io.on("connection", function (socket) {
-    console.log("connected to socket.");
+    console.log("connected to socket.io.");
     socket.emit("messages", ["Hello", "Hi there", "How are you?", "Todd Starnes is a fat fuck"])
 });
 
