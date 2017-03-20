97294a01fe5fc7cb6e5a6b3616d343132e9a4c34
Holly <hollies@hollies.pw>
2017-03-19 22:00:27 -0700

    after video 3.2

--- app/main.js
+++ app/main.js
@@ -3,4 +3,16 @@ var socket = io.connect("http://localhost", {
 });
 socket.on("messages", function (data) {
     console.info(data);
+    var html = data.map(function(d){
+        return (`
+            <div class='name'>
+                ${d.userName}
+            </div>
+            <!-- target=_blank => open in new window -->
+            <a href=${d.content.link} class='message' target=_blank>
+                ${d.content.text}</a>
+        `);
+    }).join(" ");
+
+    document.getElementById("messages").innerHTML = html;
 });
--- server/main.js
+++ server/main.js
@@ -3,12 +3,43 @@ var app = express();
 var server = require("http").Server(app);
 var port = 80;
 var io = require("socket.io")(server);
+var messages = [{
+  userId:1,
+  messageId:10,
+  userName:"Asha Greyjoy",
+  content:{
+    text:"The stone tree of the Stonetrees.",
+    link:"http://awoiaf.westeros.org/index.php/House_Stonetree"
+  },
+  likedBy:[1],
+  ts:Date.now() - 10000
+},{
+  userId:2,
+  messageId:11,
+  userName:"Arya Stark",
+  content:{
+    text:"We'll come see this inn.",
+    link:"http://gameofthrones.wikia.com/wiki/Inn_at_the_Crossroads"
+  },
+  likedBy:[2,3],
+  ts:Date.now() - 100000
+},{
+  userId:3,
+  messageId:14,
+  userName:"Cersei Lannister",
+  content:{
+    text:"Her scheming forced this on me.",
+    link:"http://gameofthrones.wikia.com/wiki/Margaery_Tyrell"
+  },
+  likedBy:[],
+  ts:Date.now() - 1000000
+}];
 
 app.use(express.static('app'));
 
 io.on("connection", function (socket) {
     console.log("connected to socket.io.");
-    socket.emit("messages", ["Hello", "Hi there", "How are you?", "Todd Starnes is a fat fuck!"])
+    socket.emit("messages", messages)
 });
 
 console.log("listen on port " + port);
