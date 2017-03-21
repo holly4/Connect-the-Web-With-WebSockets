99c8a1f73bab35c542020feef661279842e24e95
Holly <hollies@hollies.pw>
2017-03-20 20:36:49 -0700

    after video 3.6

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@
 var userId = localStorage.getItem("userId") || randomId();
 localStorage.setItem("userId", userId);
 console.info("User id: " + userId);
+var messageCache;
 
 function randomId() {
     return Math.floor(Math.random() * 1e11);
@@ -11,36 +12,57 @@ function randomId() {
 var socket = io.connect("http://localhost", {
     "forceNew": true
 });
+
 socket.on("messages", function (data) {
     console.info(data);
-    var html = data.map(function(d){
+    messageCache = data;
+    render();
+});
+
+function render() {
+    var html = messageCache.map(function (msg, index) {
         return (`
-            <div class="name">
-                ${d.userName}
-            </div>
-            <!-- target=_blank => open in new window -->
-            <a href=${d.content.link} class="message" target=_blank>
-                ${d.content.text}</a>
-            <input type="submit"  class="likes-count" value="${d.likedBy.length}  Likes"></input>
+            <form class="message" onsubmit="return likeMessage(messageCache[${index}])">
+                <div class="name">
+                    ${msg.userName}
+                </div>
+                <!-- target=_blank => open in new window -->
+                <a href=${msg.content.link} class="message" target=_blank>
+                    ${msg.content.text}</a>
+                <input type="submit"  class="likes-count" value="${msg.likedBy.length}  Likes"></input>
+            </form>
         `);
     }).join(" ");
 
     document.getElementById("messages").innerHTML = html;
-});
+}
 
 /* exported addMessage */
 function addMessage() {
-    var payload = 
-    {
-        userName : document.getElementById("username").value, 
-        content : {
-            text : document.getElementById("message").value, 
-            link : document.getElementById("linkAddress").value
+    var payload = {
+        userName: document.getElementById("username").value,
+        content: {
+            text: document.getElementById("message").value,
+            link: document.getElementById("linkAddress").value
         },
-        likedBy:[],
+        likedBy: [],
         ts: Date.now()
     }
     console.log("addMessage");
     socket.emit("new-message", payload);
+    return false;
+}
+
+/* exported likeMessage */
+function likeMessage(message) {
+    var index = message.likedBy.indexOf(userId);
+    if (index < 0) {
+        message.likedBy.push(userId);
+    } else {
+        message.likedBy.splice(index, 1);
+    }
+
+    socket.emit("update-message", message);
+    render();
     return false;
 }
